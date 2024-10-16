028aa770320ae52d7384ee90fc4d264973500134
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-10-15 21:17:56 -0300

    feat(lib): :sparkles: Remove previous code and add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'package:calorie_way/views/history.dart';
+import 'package:calorie_way/views/personal_data_form.dart';
 import 'package:flutter/material.dart';
+import 'utils/routes.dart';
+import 'views/home.dart';
 import 'utils/util.dart';
 import 'utils/theme.dart';
 
@@ -11,63 +15,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final brightness = View.of(context).platformDispatcher.platformBrightness;
-
     TextTheme textTheme = createTextTheme(context, "Roboto", "Poppins");
 
     MaterialTheme theme = MaterialTheme(textTheme);
+
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'CalorieWay',
-      theme: brightness == Brightness.light ? theme.light() : theme.dark(),
-      home: const MyHomePage(title: 'CalorieWay'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      theme: theme.light(),
+      home: const Home(),
+      routes: {
+        Routes.home: (ctx) => const Home(),
+        Routes.history: (ctx) => const History(),
+        Routes.personalDataForm: (ctx) => const PersonalDataForm(),
+      },
     );
   }
 }
