4c9a730cd942b09dc9ca69dcef88ee1e0c2bc773
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-10-16 13:39:06 -0300

    feat(lib/models): :sparkles: Add personal data model

---
+++ lib/models/personal_data_model.dart
@@ -0,0 +1,43 @@
+import 'package:calorie_way/enums/activity_level.dart';
+import 'package:calorie_way/enums/goals.dart';
+
+class PersonalDataModel {
+  final int age;
+  final int height;
+  final double weight;
+  final ActivityLevel activityLevel;
+  final Goals goals;
+  final double caloriesToLoss;
+  final double caloriesToGain;
+
+  PersonalDataModel({
+    required this.age,
+    required this.height,
+    required this.weight,
+    required this.activityLevel,
+    required this.goals,
+    required this.caloriesToLoss,
+    required this.caloriesToGain,
+  });
+
+  PersonalDataModel.fromMap(Map<String, dynamic> data)
+      : age = data['age'],
+        height = data['height'],
+        weight = data['weight'],
+        activityLevel = data['activityLevel'],
+        goals = data['goals'],
+        caloriesToLoss = data['caloriesToLoss'],
+        caloriesToGain = data['caloriesToGain'];
+
+  Map<String, Object> toMap() {
+    return {
+      'age': age,
+      'height': height,
+      'weight': weight,
+      'activityLevel': activityLevel,
+      'goals': goals,
+      'caloriesToLoss': caloriesToLoss,
+      'caloriesToGain': caloriesToGain,
+    };
+  }
+}
