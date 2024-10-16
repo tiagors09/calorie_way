5c4351811be7a636fb848007673914c824b4e8db
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-10-15 21:19:12 -0300

    feat(lib/views): :sparkles: Change app bar text and leading color, and class name

--- lib/views/personal_data_form.dart
+++ lib/views/personal_data_form.dart
@@ -1,13 +1,21 @@
 import 'package:flutter/material.dart';
 
-class History extends StatelessWidget {
-  const History({super.key});
+class PersonalDataForm extends StatelessWidget {
+  const PersonalDataForm({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Insira seus dados'),
+        leading: BackButton(
+          color: Theme.of(context).colorScheme.onPrimary,
+        ),
+        title: Text(
+          'Insira seus dados',
+          style: TextStyle(
+            color: Theme.of(context).colorScheme.onPrimary,
+          ),
+        ),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: const Placeholder(),
