81e7579eacc59b5c111d80bf92da8c512be6f113
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-10-16 16:38:00 -0300

    refactor(lib/services): :recycle: Refactor preferences

--- lib/service/preferences.dart
+++ lib/service/preferences.dart
@@ -1,15 +1,28 @@
+import 'dart:convert';
+
+import 'package:intl/intl.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Preferences {
+abstract class Preferences {
   static late SharedPreferences sp;
 
   static Future<String> savePersonalData(String data) async {
     try {
       sp = await SharedPreferences.getInstance();
 
-      final response = await sp.setString('personal_data', data);
+      final history = sp.getStringList('history') ?? [];
 
-      if (response) {
+      final dataJson = jsonDecode(data);
+      dataJson["date"] = DateFormat('dd/MM/yyyy').format(DateTime.now());
+
+      history.add(jsonEncode(dataJson));
+
+      final response = await Future.wait([
+        sp.setString('personal_data', data),
+        sp.setStringList('history', history),
+      ]);
+
+      if (response[0] && response[1]) {
         return 'Dados salvos com sucesso';
       } else {
         return 'Ocorreu um probleminha ao salvar seu dados';
@@ -19,5 +32,19 @@ class Preferences {
     }
   }
 
-  readPersonalData() async {}
+  static Future<String> readPersonalData() async {
+    try {
+      sp = await SharedPreferences.getInstance();
+
+      final response = sp.getString('personal_data');
+
+      if (response != null) {
+        return response;
+      } else {
+        throw Exception('Não há nada a ser exibido');
+      }
+    } catch (e) {
+      rethrow;
+    }
+  }
 }
