a0c7cfd0db9f3dc2f20f7a25c49dab2b2440be18
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-10-17 09:51:46 -0300

    chore: Removed windows, linux, macos platforms and optimized for web

--- lib/views/history_view.dart
+++ lib/views/history_view.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-
 import 'package:calorie_way/service/preferences.dart';
 import 'package:flutter/material.dart';
 
@@ -17,121 +16,59 @@ class _HistoryViewState extends State<HistoryView> {
 
     return Scaffold(
       appBar: AppBar(
-        leading: BackButton(
-          color: Theme.of(context).colorScheme.onPrimary,
-        ),
+        leading: BackButton(color: Theme.of(context).colorScheme.onPrimary),
         actions: [
           IconButton(
             onPressed: () {
               Preferences.cleanHistory().then((_) => setState(() {}));
             },
-            icon: Icon(
-              Icons.delete,
-              color: Theme.of(context).colorScheme.onPrimary,
-            ),
+            icon: Icon(Icons.delete,
+                color: Theme.of(context).colorScheme.onPrimary),
           ),
         ],
         title: Text(
           'Histórico',
-          style: TextStyle(
-            color: Theme.of(context).colorScheme.onPrimary,
-          ),
+          style: TextStyle(color: Theme.of(context).colorScheme.onPrimary),
         ),
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
       body: FutureBuilder(
         future: Preferences.getHistory(),
         builder: (ctx, snp) {
-          if (snp.connectionState == ConnectionState.done && snp.hasData) {
-            final history = jsonDecode(snp.data!);
-            return ListView(
-              padding: const EdgeInsets.all(24),
-              children: [
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    'Data da última checagem: ${history["date"]}',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: screenWidth * .03,
-                    ),
+          if (snp.connectionState == ConnectionState.done) {
+            if (snp.hasData) {
+              final history = jsonDecode(snp.data!);
+              return ListView(
+                padding: const EdgeInsets.all(24),
+                children: [
+                  _buildInfoTile('Data da última checagem:', history["date"]),
+                  _buildInfoTile('Peso:', '${history["weight"]} kg'),
+                  _buildInfoTile('Altura:', '${history["height"]} cm'),
+                  _buildInfoTile('Idade:', '${history["age"]} anos'),
+                  _buildInfoTile('Gênero:', history["gender"]),
+                  _buildInfoTile('Objetivo:', history["goals"]),
+                  _buildInfoTile(
+                      'Calorias a perder:', '${history["caloriesToLoss"]}'),
+                  _buildInfoTile(
+                      'Calorias a ganhar:', '${history["caloriesToGain"]}'),
+                ],
+              );
+            } else {
+              return Center(
+                child: Text(
+                  'Nenhum dado encontrado.',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: screenWidth * .06,
+                    color: Theme.of(context).colorScheme.primaryContainer,
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    'Peso ${history["weight"].toString()} kg',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: screenWidth * .03,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    'Altura ${history["height"].toString()} cm',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: screenWidth * .03,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    '${history["age"].toString()} anos de idade',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: screenWidth * .03,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    history["gender"],
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: screenWidth * .03,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    'Objetivo foi ${history["goals"]}',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: screenWidth * .03,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    'Calorias a perder ${history["caloriesToLoss"].toString()}',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: screenWidth * .03,
-                    ),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(bottom: 12),
-                  child: Text(
-                    'Calorias a ganhar ${history["caloriesToGain"].toString()}',
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontSize: screenWidth * .03,
-                    ),
-                  ),
-                ),
-              ],
-            );
+              );
+            }
           }
 
-          if (snp.connectionState == ConnectionState.done && snp.hasError) {
+          if (snp.hasError) {
             return Center(
               child: Text(
                 snp.error.toString().replaceFirst('Exception:', ''),
@@ -145,8 +82,29 @@ class _HistoryViewState extends State<HistoryView> {
             );
           }
 
-          return const CircularProgressIndicator();
+          return const Center(child: CircularProgressIndicator());
         },
+      ),
+    );
+  }
+
+  Widget _buildInfoTile(String label, String value) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            label,
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.primary, fontSize: 16),
+          ),
+          Text(
+            value,
+            style: TextStyle(
+                color: Theme.of(context).colorScheme.primary, fontSize: 16),
+          ),
+        ],
       ),
     );
   }
