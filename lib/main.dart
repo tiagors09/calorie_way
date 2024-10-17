851328dc3faaee71347f84bb6527e87948f4a09e
Tiago Rodrigues Sousa <tiagorodriguessousa9@gmail.com>
2024-10-17 09:11:15 -0300

    feat: :sparkles: Add resoruces to splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:calorie_way/views/personal_data_form_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'utils/routes.dart';
 import 'views/history_view.dart';
 import 'views/home_view.dart';
@@ -7,11 +8,25 @@ import 'utils/util.dart';
 import 'utils/theme.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
